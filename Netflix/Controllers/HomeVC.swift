50f7fa455daa2406ec5abc5a1710eed5076e8be4
pratapsinghrana1999 <pratapsinghrana1999@gmail.com>
2023-09-25 13:31:57 +0530

    Step 2 :- 1. Added TableView in HomeVC. 	> TableView has a big table header, multiple sections (only one table cell in one section). 	> Each section has a section header of 40 px. 2. Added CollectionView as CollectionViewTableViewCell.swift in tableView cells. 3. Added MVVM architecture folders.

--- Netflix/Controllers/HomeVC.swift
+++ Netflix/Controllers/HomeVC.swift
@@ -9,8 +9,52 @@ import UIKit
 
 class HomeVC: UIViewController {
 
+    private let homeFeedTable : UITableView = {
+        let table = UITableView(frame: .zero, style: .grouped)
+        table.register(CollectionViewTableViewCell.self , forCellReuseIdentifier: "CollectionViewTableViewCell")
+        return table
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        
+        //Add Subviews.
+        view.addSubview(homeFeedTable)
+        
+        homeFeedTable.dataSource = self
+        homeFeedTable.delegate = self
+        homeFeedTable.tableHeaderView = UIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        homeFeedTable.frame = view.bounds
+    }
+}
+
+extension HomeVC : UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CollectionViewTableViewCell.identifier, for: indexPath) as? CollectionViewTableViewCell else {
+            return UITableViewCell()
+        }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 40
     }
 }
---
+++ Netflix/Views/CollectionViewTableViewCell.swift
@@ -0,0 +1,59 @@
+//
+//  CollectionViewTableViewCell.swift
+//  Netflix
+//
+//  Created by Apple on 25/09/23.
+//
+
+import UIKit
+
+class CollectionViewTableViewCell: UITableViewCell {
+    
+    static let identifier = "CollectionViewTableViewCell"
+    
+    //CollectionView Creation.
+    private let collectionView : UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: 140, height: 200)
+        layout.scrollDirection = .horizontal
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        return collectionView
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        //TableCell Properties
+        contentView.backgroundColor = .systemYellow
+        
+        //Add Subviews
+        contentView.addSubview(collectionView)
+        
+        //CollectionView Properties
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        collectionView.frame = contentView.bounds
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension CollectionViewTableViewCell : UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .green
+        return cell
+    }
+}
